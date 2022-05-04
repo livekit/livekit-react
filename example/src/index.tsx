b17aa12ab23f0bbae199dd90de1dafd1fb1a20bb
David Zhao <dz@livekit.io>
2022-05-03 23:58:03 -0700

    Support for React 18 (#44)
    * comments about strict-mode

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,5 +1,6 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root') as Element)
+root.render(<App />)
